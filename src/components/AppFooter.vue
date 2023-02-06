f2f3dd165ffeff5f7844f70247ce4937eeb52123
Andrea Raguso <info@andrearaguso.com>
2023-02-06 20:51:44 +0100

    Header e main finito

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'La mia prima app con Vite!'
+    };
+  },
+};
+</script>
+
+<template>
+  <!-- <div class="parent">
+    <img class="image1" src="../img/footer-bg.jpg" />
+    <img class="image2" src="../img/dc-logo-bg.png" />
+  </div> -->
+  <div class="container">
+    <div>
+      <ul>
+        <li>DC COMICS</li>
+        <li>Characters</li>
+        <li>Comics</li>
+        <li>Movies</li>
+        <li>TV</li>
+        <li>Games</li>
+        <li>Videos</li>
+        <li>News</li>
+      </ul>
+      <ul>
+        <li>SHOP</li>
+        <li>Shop DC</li>
+        <li>Shop DC Collectibles</li>
+      </ul>
+      <ul>
+        <li>DC</li>
+        <li>Terns Of USe</li>
+        <li>Privacy policy (New)</li>
+        <li>Advertising</li>
+        <li>Jobs</li>
+        <li>Subscriptions</li>
+        <li>Talent Workshops</li>
+        <li>CPSC Certificates</li>
+        <li>Ratings</li>
+        <li>Shop Help</li>
+        <li>Contact Us</li>
+      </ul>
+      <ul>
+        <li>SITES</li>
+        <li>DC</li>
+        <li>MAD Magazine</li>
+        <li>DC Kids</li>
+        <li>DC Universe</li>
+        <li>DC Power Visa</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style  scoped>
+@import '../styles/partials/AppFooter.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="container d-flex align-items-center justify-content-between">
+      <div>
+          <img src="../img/dc-logo.png"  alt="">
+      </div>
+      <div class="">
+          <ul class="d-flex align-items-center" >
+              <li>CHARACTERS</li>
+              <li class="active">COMICS</li>
+              <li>MOVIES</li>
+              <li>TV</li>
+              <li>GAMES</li>
+              <li>COLLECTIBLES</li>
+              <li>VIDEOS</li>
+              <li>FANS</li>
+              <li>NEWS</li>
+              <li>SHOP</li>
+          </ul>
+      </div>
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+@import '../styles/partials/AppHeader.scss'
+</style>
