10f08ec53c82014ed36fb556e32eafe84a791c5c
Andrea Raguso <info@andrearaguso.com>
2023-02-07 07:01:44 +0100

    footer finito

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,48 +13,72 @@ export default {
     <img class="image1" src="../img/footer-bg.jpg" />
     <img class="image2" src="../img/dc-logo-bg.png" />
   </div> -->
-  <div class="container">
-    <div>
-      <ul>
-        <li>DC COMICS</li>
-        <li>Characters</li>
-        <li>Comics</li>
-        <li>Movies</li>
-        <li>TV</li>
-        <li>Games</li>
-        <li>Videos</li>
-        <li>News</li>
-      </ul>
-      <ul>
-        <li>SHOP</li>
-        <li>Shop DC</li>
-        <li>Shop DC Collectibles</li>
-      </ul>
-      <ul>
-        <li>DC</li>
-        <li>Terns Of USe</li>
-        <li>Privacy policy (New)</li>
-        <li>Advertising</li>
-        <li>Jobs</li>
-        <li>Subscriptions</li>
-        <li>Talent Workshops</li>
-        <li>CPSC Certificates</li>
-        <li>Ratings</li>
-        <li>Shop Help</li>
-        <li>Contact Us</li>
-      </ul>
-      <ul>
-        <li>SITES</li>
-        <li>DC</li>
-        <li>MAD Magazine</li>
-        <li>DC Kids</li>
-        <li>DC Universe</li>
-        <li>DC Power Visa</li>
-      </ul>
+  <div class="top-f">
+    <div class="d-flex container">
+      <div class="container d-flex py-4">
+        <div>
+          <ul>
+            <li class=" fw-bold text-white fs-5 mb-2">DC COMICS</li>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+          <ul>
+            <li class=" fw-bold text-white fs-5 mb-2">SHOP</li>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
+        </div>
+        <ul>
+          <li class=" fw-bold text-white fs-5 mb-2">DC</li>
+          <li>Terns Of USe</li>
+          <li>Privacy policy (New)</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certificates</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+          <li>Contact Us</li>
+        </ul>
+        <ul>
+          <li class=" fw-bold text-white fs-5 mb-2">SITES</li>
+          <li>DC</li>
+          <li>MAD Magazine</li>
+          <li>DC Kids</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+
+      <div>
+        <img src="../img/dc-logo-bg.png" class=" img-fluid" alt="">
+      </div>
+
+    </div>
+  </div>
+  <div class="f-bottom">
+    <div class="container d-flex align-items-center h-100 justify-content-between">
+      <div class="sign">
+        <a href="#">SIGN UP NOW!</a>
+      </div>
+      <div>
+        <span class="follow fw-bold fs-5">FOLLOW US</span>
+        <img src="../img/footer-facebook.png" alt="" class="ms-3">
+        <img src="../img/footer-twitter.png" alt="" class="ms-3">
+        <img src="../img/footer-youtube.png" alt="" class="ms-3">
+        <img src="../img/footer-pinterest.png" alt="" class="ms-3">
+        <img src="../img/footer-periscope.png" alt="" class="ms-3">
+      </div>
     </div>
   </div>
 </template>
 
-<style  scoped>
+<style scoped>
 @import '../styles/partials/AppFooter.scss'
 </style>
